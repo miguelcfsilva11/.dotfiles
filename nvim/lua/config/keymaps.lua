09ca3391c2959c9eded0d3fa1258e51fb516e91b
Miguel Silva <miguelsilva@MacBook-Pro-de-Miguel.local>
2025-10-20 19:10:14 +0100

    fix for buffer swapping and conflict for file explorer

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,3 +7,7 @@ local map = vim.keymap.set
 
 map({"n", "v"}, "<C-j>", "<cmd>norm 6j<cr>")
 map({"n", "v"}, "<C-k>", "<cmd>norm 6k<cr>")
+
+-- Buffer navigation
+map("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,7 +2,7 @@ return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
-		default_file_explorer = true,
+		default_file_explorer = false,
 		columns = {
 			"icon",
 			-- "permissions",
